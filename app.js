9dbfaed021558687d14947de84faec4f8dad7d71
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2016-06-24 22:30:41 -0600

    Improved carousel to show previous and next item

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 var main = function() {
-  $(".btn-projects").click(function() {
-      $('html,body').animate({
-          scrollTop: $(".carousel").offset().top},
-          'slow');
-  });
-  $(".btn-about").click(function() {
-      $('html,body').animate({
-          scrollTop: $(".bio").offset().top},
-          'slow');
-  });
+    $(".btn-projects").click(function() {
+        $('html,body').animate({
+                scrollTop: $(".proj").offset().top
+            },
+            'slow');
+    });
+    $(".btn-about").click(function() {
+        $('html,body').animate({
+                scrollTop: $(".bio").offset().top
+            },
+            'slow');
+    });
+    $('#myCarousel').carousel({
+        interval: 10000
+    });
+    $('.carousel .item').each(function() {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+        if (next.next().length > 0) {
+            next.next().children(':first-child').clone().appendTo($(this));
+        } else {
+            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+        }
+    });
 };
 
 $(document).ready(main);
